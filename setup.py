57613cf6cf026d6c1efbd26ee198ce580a972483
Iuri Guilherme <2027447+iuriguilherme@users.noreply.github.com>
2022-10-14 06:27:21 -0300

    Atualizando tags

--- setup.py
+++ setup.py
@@ -1,2 +1,3 @@
 from setuptools import setup
-setup(dependency_links=['git+https://github.com/iuriguilherme/iacecil.git@v0.2.4#egg=iacecil'],install_requires=['pipenv-setup', 'pydantic[dotenv]'],)
+setup()
+
