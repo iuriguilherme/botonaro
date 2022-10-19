7ad0e60b1fcd248705ca0133faeedeed650c0ee3
Iuri Guilherme <2027447+iuriguilherme@users.noreply.github.com>
2022-10-18 21:57:03 -0300

    Busca na base de frases

---
+++ src/instance/personalidades/custom.py
@@ -0,0 +1,435 @@
+"""Personalidade ia.cecil: Botonaro"""
+
+import logging
+logger = logging.getLogger(__name__)
+
+import aiohttp
+import bs4
+import datetime
+import os
+import random
+import typing
+from aiogram import (
+    Dispatcher,
+    filters,
+    types,
+)
+from iacecil.controllers.aiogram_bot.callbacks import (
+    command_callback,
+    message_callback,
+    error_callback,
+)
+from iacecil.controllers.util import (
+    dice_high,
+    dice_low,
+)
+
+class ResultMixin(object):
+    """Modelo para resultado da busca"""
+    link: str
+    titulo: str
+    fonte: str
+    data: datetime.datetime
+    metamemo: str
+    url: str
+    midia: list[str]
+    def __repr__(self):
+        return f"""<ResultMixin(\
+link: {self.link}, \
+titulo: {self.titulo}, \
+fonte: {self.fonte}, \
+data: {str(self.data)}, \
+metamemo: {self.metamemo}, \
+url: {self.url}, \
+midia: {",".join(self.midia)}\
+>"""
+
+async def html_to_bytes(url: str) -> typing.Union[bytes, None]:
+    """Performs async HTTP GET on URL and returns bytes for parsing"""
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url) as response:
+                return await response.read()
+    except Exception as e:
+        logger.exception(e)
+        return None
+
+async def html_to_json(url: str) -> typing.Union[str, None]:
+    """Performs async HTTP GET on URL and returns JSON string"""
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url) as response:
+                return await response.json()
+    except Exception as e:
+        logger.exception(e)
+        return None
+
+async def bytes_to_soup(html: bytes) -> typing.Union[bs4.BeautifulSoup, None]:
+    """Parse bytes to beautiful soup"""
+    try:
+        return bs4.BeautifulSoup(html, "lxml")
+    except bs4.FeatureNotFound:
+        logger.debug("python-lxml not installed, falling back to html.parser")
+        return bs4.BeautifulSoup(html, "html.parser")
+    except Exception as e:
+        logger.exception(e)
+        return None
+
+async def parse_result(result_item: bs4.element.Tag) -> ResultMixin:
+    """Parse HTML into Python Object"""
+    try:
+        result: ResultMixin = ResultMixin()
+        ## Nota do desenvolvedor: eu ODEIO isto. Isso é a coisa mais feia, 
+        ## chata e imbecil que existe no mundo da programação. É muito fácil 
+        ## editar o HTML que é gerado e quebrar totalmente o modelo que 
+        ## esta função cria, me forçando a abrir o navegador, o terminal do 
+        ## python e a IDE pra reescrever tudo de novo, do zero. Este aqui é o 
+        ## melhor exemplo de porque é que uma coisa que "está funcionando" não 
+        ## significa sob hipótese alguma que daqui a alguns segundos ainda vai 
+        ## estar "funcionando". Isso aqui não é programar, isso aqui é brincar 
+        ## de ficar fazendo gambiarra. No pior sentido. Isso aqui NÃO presta, 
+        ## NÃO funciona, NÃO resolve, NÃO soluciona. Só causa problemas e toma 
+        ## tempo desnecessariamente. O nome das tags mudou pelo menos uma vez 
+        ## durante a fase de testes desse conjunto de subrotinas. 
+        ## RESTful API or GTFO. </rant>
+        columns: bs4.elements.ResultSet = result_item.find_all('div', 's1')
+        result.link: str = columns[0].find('a', 'icon-eye').get('href')
+        result.titulo: str = result_item.find('div', 's5').text
+        result.fonte: str = columns[1].find('i').get('class')[1].split('-'
+            )[1].strip('1').capitalize()
+        result.data: datetime.datetime = datetime.datetime.strptime(
+            " ".join([columns[2].text, columns[3].text]), "%d/%m/%Y %H:%M")
+        result.metamemo: str = columns[4].text
+        result.url: str = columns[5].find('a').get('href')
+        result.midia: list[str] = [a.get('href') for a in \
+            columns[6].find_all('a')]
+        logger.debug(result)
+        return result
+    except Exception as e:
+        logger.exception(e)
+        return None
+
+async def busca_frase(palavras: list[str]) -> list[ResultMixin]:
+    """Retorna o resultado de uma busca em FONTE_URL"""
+    try:
+        fonte_url: str = os.environ.get('FONTE_URL')
+        sources: list = os.environ.get('SOURCES_BUSCA').split(',')
+        start_date: str = os.environ.get('START_DATE_BUSCA')
+        end_date: str = datetime.datetime.today().strftime("%Y-%m-%d")
+        url: str = f"{fonte_url}/?" \
+            + "&".join([f"source={source}" for source in sources]) \
+            + f"&content={'%20'.join(palavras)}" \
+            + f"&start_date={start_date}" \
+            + f"&end_date={end_date}"
+        # ~ logger.debug(f"url = {url}")
+        soup: BeautifulSoup = await bytes_to_soup(await html_to_bytes(url))
+        # ~ logger.debug(f"len(soup) = {len(soup)}")
+        # ~ logger.debug(f"soup = {str(soup)}")
+        results: bs4.element.ResultSet = soup.find('div', 'search-results',
+            ).find_all('div', 'result-item')
+        # ~ logger.debug(f"len(results) = {len(results)}")
+        retornos: list = [await parse_result(result) for result in results]
+        # ~ logger.debug(f"""len(retornos) = {len(retornos)}\nretornos.titulo = {[
+            # ~ retorno.titulo for retorno in retornos]}""")
+        try:
+            retornos[1]
+        except IndexError:
+            raise
+        return retornos
+    except Exception as e:
+        logger.exception(e)
+        ## TODO: responder isto somente se tiver 0 resultados na busca
+        retorno: ResultMixin = ResultMixin()
+        retorno.titulo: str = """não me recordo de nada no tocante a essa \
+qüestão aí talquei"""
+        retornos: list = [retorno]
+        # ~ logger.debug(f"""len(retornos) = {len(retornos)}\nretornos.titulo = {[
+            # ~ retorno.titulo for retorno in retornos]}""")
+        return retornos
+
+async def add_instance_handlers(dispatcher: Dispatcher) -> None:
+    """Registra handlers para aiogram.Dispatcher, lida com Telegram"""
+    try:
+        ## TODO compreensão de lista e import_module URGENTE
+        ## Estes módulos são gerados por outros scripts em tempo de execução,
+        ## não tem como manter o código em versionamento (ou tem?)
+        from geracao_1 import (
+            gatilhos_1,
+            gatilhos_2,
+            gatilhos_3,
+            gatilhos_5,
+            gatilhos_6,
+            gatilhos_7,
+            gatilhos_8,
+            respostas_1,
+            respostas_2,
+            respostas_3,
+            respostas_4,
+            respostas_5,
+            respostas_6,
+            respostas_7,
+            respostas_8,
+        )
+        @dispatcher.message_handler(is_reply_to_id = dispatcher.bot.id)
+        async def palavras_4_callback(message: types.Message) -> None:
+            try:
+                descriptions: list = [
+                    'botonaro',
+                    'geracao1',
+                    'gatilho4',
+                    dispatcher.config.personalidade,
+                    message.chat.type,
+                ] # descriptions
+                await message_callback(message, descriptions)
+                await command_callback(await message.reply(
+                    await respostas_4()), descriptions)
+            except Exception as e1:
+                logger.exception(e1)
+                await error_callback("Erro treplicando", message,
+                    e1, ['exception'] + descriptions)
+        @dispatcher.message_handler(
+            filters.Regexp(r'\b({})\b'.format('|'.join(await gatilhos_1()))),
+            # ~ is_reply_to_id = dispatcher.bot.id,
+        )
+        async def palavras_1_callback(message: types.Message) -> None:
+            """Geração 1 de gatilhos e respostas, gatilhos 1"""
+            try:
+                descriptions: list = [
+                    'botonaro',
+                    'geracao1',
+                    'gatilho1',
+                    dispatcher.config.personalidade,
+                    message.chat.type,
+                ] # descriptions
+                await message_callback(message, descriptions)
+                # ~ command: Union[types.Message, None] = None
+                # ~ if (await dice_low(15)):
+                    # ~ command = await message.reply(await respostas_1())
+                # ~ if command is not None:
+                    # ~ await command_callback(command, descriptions)
+                await command_callback(await message.reply(
+                    await respostas_1()), descriptions)
+            except Exception as e1:
+                logger.exception(e1)
+                await error_callback("Erro respondendo gatilho", message,
+                    e1, ['exception'] + descriptions)
+        @dispatcher.message_handler(
+            filters.Regexp(r'\b({})\b'.format('|'.join(await gatilhos_2()))),
+            is_reply_to_id = dispatcher.bot.id,
+        )
+        async def palavras_2_callback(message: types.Message) -> None:
+            """Geração 1 de gatilhos e respostas, gatilhos 2"""
+            try:
+                descriptions: list = [
+                    'botonaro',
+                    'geracao1',
+                    'gatilho2',
+                    dispatcher.config.personalidade,
+                    message.chat.type,
+                ] # descriptions
+                await message_callback(message, descriptions)
+                await command_callback(await message.reply(
+                    await respostas_2()), descriptions)
+            except Exception as e1:
+                logger.exception(e1)
+                await error_callback("Erro respondendo gatilho", message,
+                    e1, ['exception'] + descriptions)
+        @dispatcher.message_handler(
+            filters.Regexp(r'\b({})\b'.format('|'.join(await gatilhos_3()))),
+        )
+        async def palavras_3_callback(message: types.Message) -> None:
+            """Geração 1 de gatilhos e respostas, gatilhos 3"""
+            try:
+                descriptions: list = [
+                    'botonaro',
+                    'geracao1',
+                    'gatilho3',
+                    dispatcher.config.personalidade,
+                    message.chat.type,
+                ] # descriptions
+                await message_callback(message, descriptions)
+                await command_callback(await message.reply(
+                    await respostas_3()), descriptions)
+            except Exception as e1:
+                logger.exception(e1)
+                await error_callback("Erro respondendo gatilho", message,
+                    e1, ['exception'] + descriptions)
+        @dispatcher.message_handler(
+            filters.Regexp(r'\b({})\b'.format('|'.join(await gatilhos_5()))),
+        )
+        async def palavras_5_callback(message: types.Message) -> None:
+            """Geração 1 de gatilhos e respostas, gatilhos 5"""
+            try:
+                descriptions: list = [
+                    'botonaro',
+                    'geracao1',
+                    'gatilho5',
+                    dispatcher.config.personalidade,
+                    message.chat.type,
+                ] # descriptions
+                await message_callback(message, descriptions)
+                await command_callback(await message.reply(
+                    await respostas_5()), descriptions)
+            except Exception as e1:
+                logger.exception(e1)
+                await error_callback("Erro respondendo gatilho", message,
+                    e1, ['exception'] + descriptions)
+        @dispatcher.message_handler(
+            filters.Regexp(r'\b({})\b'.format('|'.join(await gatilhos_6()))),
+        )
+        async def palavras_6_callback(message: types.Message) -> None:
+            """Geração 1 de gatilhos e respostas, gatilhos 6"""
+            try:
+                descriptions: list = [
+                    'botonaro',
+                    'geracao1',
+                    'gatilho6',
+                    dispatcher.config.personalidade,
+                    message.chat.type,
+                ] # descriptions
+                await message_callback(message, descriptions)
+                await command_callback(await message.reply(
+                    await respostas_6()), descriptions)
+            except Exception as e1:
+                logger.exception(e1)
+                await error_callback("Erro respondendo gatilho", message,
+                    e1, ['exception'] + descriptions)
+        @dispatcher.message_handler(
+            filters.Regexp(r'\b({})\b'.format('|'.join(await gatilhos_7()))),
+        )
+        async def palavras_7_callback(message: types.Message) -> None:
+            """Geração 1 de gatilhos e respostas, gatilhos 7"""
+            try:
+                descriptions: list = [
+                    'botonaro',
+                    'geracao1',
+                    'gatilho7',
+                    dispatcher.config.personalidade,
+                    message.chat.type,
+                ] # descriptions
+                await message_callback(message, descriptions)
+                await command_callback(await message.reply(
+                    await respostas_7()), descriptions)
+            except Exception as e1:
+                logger.exception(e1)
+                await error_callback("Erro respondendo gatilho", message,
+                    e1, ['exception'] + descriptions)
+        @dispatcher.message_handler(
+            filters.Regexp(r'\b({})\b'.format('|'.join(await gatilhos_8()))),
+        )
+        async def palavras_8_callback(message: types.Message) -> None:
+            """Geração 1 de gatilhos e respostas, gatilhos 8"""
+            try:
+                descriptions: list = [
+                    'botonaro',
+                    'geracao1',
+                    'gatilho8',
+                    dispatcher.config.personalidade,
+                    message.chat.type,
+                ] # descriptions
+                await message_callback(message, descriptions)
+                await command_callback(await message.reply(
+                    await respostas_8()), descriptions)
+            except Exception as e1:
+                logger.exception(e1)
+                await error_callback("Erro respondendo gatilho", message,
+                    e1, ['exception'] + descriptions)
+        @dispatcher.message_handler(filters.CommandStart())
+        async def start_callback(message: types.Message) -> None:
+            """Responde /start"""
+            try:
+                descriptions: list = [
+                    'botonaro',
+                    'geracao1',
+                    'start',
+                    dispatcher.config.personalidade,
+                    message.chat.type,
+                ] # descriptions
+                await message_callback(message, descriptions)
+                await command_callback(await message.reply(
+                    await respostas_6()), descriptions)
+            except Exception as e1:
+                logger.exception(e1)
+                await error_callback("Erro respondendo gatilho", message,
+                    e1, ['exception'] + descriptions)
+    except Exception as e:
+        logger.error("Arquivos não foram gerados corretamente")
+        logger.exception(e)
+    try:
+        @dispatcher.message_handler(commands = ['sobre', 'm'])
+        async def busca_comando_callback(message: types.Message) -> None:
+            """Busca através dos argumentos do comando"""
+            descriptions: list = [
+                'botonaro',
+                'buscamemo',
+                'buscacomando',
+                dispatcher.config.personalidade,
+                message.chat.type,
+            ] # descriptions
+            await message_callback(message, descriptions)
+            try:
+                await command_callback(
+                    await message.reply(
+                        text = random.choice(
+                            await busca_frase(message.get_args().split(' ')),
+                        ).titulo, # text
+                    ), # reply
+                    descriptions,
+                ) # command_callback
+            except Exception as e1:
+                logger.exception(e1)
+                await error_callback("Erro buscando frase", message,
+                    e1, ['exception'] + descriptions)
+        async def busca_natural(message: types.Message) -> None:
+            """Busca através de palavras na mensagem"""
+            try:
+                descriptions: list = [
+                    'botonaro',
+                    'buscamemo',
+                    'buscanatural',
+                    dispatcher.config.personalidade,
+                    message.chat.type,
+                ] # descriptions
+                await message_callback(message, descriptions)
+                await command_callback(
+                    await message.reply(
+                        text = random.choice(await busca_frase([
+                            termo \
+                            for termo in \
+                            message.text.split(' ') \
+                            if termo not in \
+                            ['fala', 'sobre']
+                        ])).titulo, # text
+                    ), # reply
+                    descriptions,
+                ) # command_callback
+            except Exception as e1:
+                logger.exception(e1)
+                await error_callback("Erro buscando frase", message,
+                    e1, ['exception'] + descriptions)
+        @dispatcher.message_handler(filters.Regexp(r'\bfala sobre\b'))
+        async def busca_natural_callback(message: types.Message) -> None:
+            await busca_natural(message)
+        @dispatcher.message_handler(
+            filters.ChatTypeFilter(types.ChatType.PRIVATE),
+        )
+        async def busca_private_callback(message: types.Message) -> None:
+            await busca_natural(message)
+        @dispatcher.message_handler(
+            content_types = types.ContentTypes.ANY,
+            state = "*",
+        )
+        async def chance_busca_callback(message: types.Message) -> None:
+            """Responde em uma chance aleatória"""
+            try:
+                if await dice_low(int(os.environ.get("CHANCE", 30))):
+                    await busca_natural(message)
+                else:
+                    logger.debug("sem resposta")
+            except Exception as e1:
+                logger.exception(e1)
+                await error_callback("Erro buscando frase", message,
+                    e1, ['exception'])
+    except Exception as e:
+        logger.error("Não consegui registrar os handlers de busca")
+        logger.exception(e)
