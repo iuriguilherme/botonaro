a6addec23d2d03d67390c5cb19c14b2ab08c79f7
Iuri Guilherme <2027447+iuriguilherme@users.noreply.github.com>
2022-12-14 22:58:30 -0300

    Javascript do Modal

---
+++ doc/index.js
@@ -0,0 +1,14 @@
+/*
+ *  Botonaro  
+ * Copyright 2022 Iuri Guilherme <https://iuri.neocities.org/>  
+ * Creative Commons 4.0 Attribution Share Alike  
+*/
+
+// https://getbootstrap.com/docs/5.2/components/modal/
+const imageModal = document.getElementById('imageModal')
+imageModal.addEventListener('show.bs.modal', event => {
+    const trigger = event.relatedTarget
+    const image = trigger.getAttribute('src')
+    const modalImage = imageModal.querySelector('.modal-body img')
+    modalImage.src = `${image}`
+})
